09bdd6ad9eb5bc5174c39fb034bb24a8f8c2258f
Mark Adams <mark@thoughtbot.com>
2012-10-18 17:21:41 -0700

    Fix require statements

--- lib/liftoff/commands.rb
+++ lib/liftoff/commands.rb
@@ -1,8 +1,7 @@
+$:.push File.expand_path('../', __FILE__)
+
 require 'commands/all'
 require 'commands/git'
 require 'commands/releasewarnings'
 require 'commands/todo'
 require 'commands/warnings'
-
-$:.push File.expand_path('../', __FILE__)
-
