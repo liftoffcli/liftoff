5fc02333b2bcfcafa9d115c548d2beec0012d4f5
Gordon Fontenot <gordon@thoughtbot.com>
2013-01-18 11:09:40 -0500

    Clean up commands

--- lib/liftoff/commands/git.rb
+++ lib/liftoff/commands/git.rb
@@ -1,9 +1,9 @@
 command :git do |c|
-  c.syntax = 'liftoff git [options]'
+  c.syntax = 'liftoff git'
   c.summary = 'Add default .gitignore and .gitattributes files.'
   c.description = ''
 
-  c.action do |args, options|
+  c.action do
     GitHelper.new.generate_files
   end
 end
--- lib/liftoff/commands/releasewarnings.rb
+++ lib/liftoff/commands/releasewarnings.rb
@@ -1,9 +1,9 @@
 command :releasewarnings do |c|
-  c.syntax = 'liftoff releasewarnings [options]'
+  c.syntax = 'liftoff releasewarnings'
   c.summary = 'Treat all warnings as errors in release schemes.'
   c.description = ''
 
-  c.action do |args, options|
+  c.action do
     XcodeprojHelper.new.treat_warnings_as_errors
   end
 end
