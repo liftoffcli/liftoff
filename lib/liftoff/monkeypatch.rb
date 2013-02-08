0adf87bc0dea84e3dac870069869184315191d3e
Gordon Fontenot <gordon@thoughtbot.com>
2013-02-08 13:54:02 -0500

    Add support for multiple projects and targets
    Mokeypatching Xcodeproj right now to simplify the code for choosing
    targets. I am going to be submitting a PR to them to do this properly in
    a bit. Then when we upgrade Xcodeproj as part of #24, we can remove this
    as well.
    
    Fixes #21

---
+++ lib/liftoff/monkeypatch.rb
@@ -0,0 +1,13 @@
+# Temp fix while waiting for an update to Xcodeproj
+# https://github.com/CocoaPods/Xcodeproj/pull/50
+module Xcodeproj
+  class Project
+    module Object
+      class PBXNativeTarget
+        def to_s
+          name
+        end
+      end
+    end
+  end
+end
