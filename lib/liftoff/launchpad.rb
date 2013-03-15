1661a7acf78a9b9502822b0f85cff225fb265787
Gordon Fontenot <gordon@thoughtbot.com>
2013-03-15 18:00:37 -0400

    Replace Commander with Slop
    Commander was too inflexible for our purposes, and lead to duplication.
    Slop is a much nicer and lighter weight command line parser. As a side
    effect, this also allows users to run multiple individual commands.
    
    Fixes #28

---
+++ lib/liftoff/launchpad.rb
@@ -0,0 +1,105 @@
+module Liftoff
+  class LaunchPad
+    def initialize(argv)
+      parse_options(argv)
+    end
+
+    def liftoff
+      if @opts[:help]
+        display_help
+      elsif @opts[:version]
+        display_version
+      else
+        if @opts[:git]
+          generate_git
+        end
+
+        if @opts[:indentation]
+          set_indentation_level
+        end
+
+        if @opts[:errors]
+          treat_warnings_as_errors
+        end
+
+        if @opts[:todo]
+          add_todo_script_phase
+        end
+
+        if @opts[:warnings]
+          enable_hosey_warnings
+        end
+
+        if @opts[:staticanalyzer]
+          enable_static_analyzer
+        end
+      end
+    end
+
+    private
+
+    def parse_options(argv)
+      @opts = Slop.parse(argv, :strict => true) do
+        on :v, :version, 'Print the version'
+        on :a, :all, 'Run all commands (Default)'
+        on :g, :git, 'Add default .gitignore and .gitattributes files'
+        on :i, :indentation=, 'Set the indentation level (in spaces, defaults to 4)', :argument => :optional, :as => Integer do |user_indentation_level|
+          fetch_option(:indentation).value = user_indentation_level || DEFAULT_INDENTATION_LEVEL
+        end
+        on :e, :error, 'Treat warnings as errors (Only for release configurations)'
+        on :t, :todo, 'Add a build script to turn TODO and FIXME comments into warnings'
+        on :w, :warnings, 'Turn on Hosey warnings at the project level'
+        on :s, :staticanalyzer, 'Turn on static analysis for the project'
+        on :h, :help, 'Display this help message'
+      end
+
+      if @opts.to_hash.values.compact.empty? || @opts[:all]
+        turn_on_all_options
+      end
+    end
+
+    def display_help
+      puts @opts.help
+    end
+
+    def display_version
+      puts "Version: #{Liftoff::VERSION}"
+    end
+
+    def generate_git
+      GitHelper.new.generate_files
+    end
+
+    def set_indentation_level
+      xcode_helper.set_indentation_level(@opts[:indentation])
+    end
+
+    def treat_warnings_as_errors
+      xcode_helper.treat_warnings_as_errors
+    end
+
+    def add_todo_script_phase
+      xcode_helper.add_todo_script_phase
+    end
+
+    def enable_hosey_warnings
+      xcode_helper.enable_hosey_warnings
+    end
+
+    def enable_static_analyzer
+      xcode_helper.enable_static_analyzer
+    end
+
+    def xcode_helper
+      @xcode_helper ||= XcodeprojHelper.new
+    end
+
+    def turn_on_all_options
+      %w(git error todo warnings staticanalyzer).each do |option|
+        @opts.fetch_option(option.to_sym).value = true
+      end
+
+      @opts.fetch_option(:indentation).value ||= DEFAULT_INDENTATION_LEVEL
+    end
+  end
+end
--- lib/liftoff/xcodeproj_helper.rb
+++ lib/liftoff/xcodeproj_helper.rb
@@ -33,13 +33,13 @@ class XcodeprojHelper
   end
 
   def treat_warnings_as_errors
-    say 'Setting GCC_TREAT_WARNINGS_AS_ERRORS for Release builds'
+    puts 'Setting GCC_TREAT_WARNINGS_AS_ERRORS for Release builds'
     @target.build_settings('Release')['GCC_TREAT_WARNINGS_AS_ERRORS'] = 'YES'
     save_changes
   end
 
   def enable_hosey_warnings
-    say 'Setting Hosey warnings at the project level'
+    puts 'Setting Hosey warnings at the project level'
     @project.build_configurations.each do |configuration|
       HOSEY_WARNINGS.each do |setting|
         configuration.build_settings[setting] = 'YES'
@@ -49,7 +49,7 @@ class XcodeprojHelper
   end
 
   def enable_static_analyzer
-    say 'Turning on Static Analyzer at the project level'
+    puts 'Turning on Static Analyzer at the project level'
     @project.build_configurations.each do |configuration|
       configuration.build_settings['RUN_CLANG_STATIC_ANALYZER'] = 'YES'
     end
@@ -57,7 +57,7 @@ class XcodeprojHelper
   end
 
   def set_indentation_level(level)
-    say "Setting the project indentation level to #{level} spaces"
+    puts "Setting the project indentation level to #{level} spaces"
     main_group = @project.main_group
     main_group.indent_width = level.to_s
     main_group.tab_width = level.to_s
@@ -66,7 +66,7 @@ class XcodeprojHelper
   end
 
   def add_todo_script_phase
-    say 'Adding shell script build phase to warn on TODO and FIXME comments'
+    puts 'Adding shell script build phase to warn on TODO and FIXME comments'
     add_shell_script_build_phase(TODO_WARNING_SCRIPT, 'Warn for TODO and FIXME comments')
   end
 
