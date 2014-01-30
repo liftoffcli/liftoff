aa24e57da04e254f8f1450ce8274bb176672d9fa
Gio <giovanni.lodi42@gmail.com>
2014-01-30 09:06:01 +0000

    filter_valid_options is not an instance method, do not use #

--- spec/options_helper_spec.rb
+++ spec/options_helper_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require 'liftoff'
 
 describe OptionsHelper do
-  describe "#filter_valid_options" do 
+  describe "filter_valid_options" do 
     it "filters invalid options from a list of options" do
       any_valid_options = { :git => true, "error" => true }
       any_invalid_options = { :invalid_options => "pizza maragherita", :invalid_number => 42 }
