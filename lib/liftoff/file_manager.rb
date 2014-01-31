f0c5e7c09d6caaf6f254d22fec5787ecf45e2fde
Gordon Fontenot <gordon@thoughtbot.com>
2014-01-30 21:54:04 -0500

    Replace GitHelper with FileManager
    FileManager will take any template file (located in /lib/templates) and
    move it into the current directory. If it finds an existing file at that
    location, it appends the contents of the existing file to the template
    after it installs the template.

---
+++ lib/liftoff/file_manager.rb
@@ -0,0 +1,47 @@
+require 'fileutils'
+
+class FileManager
+  def create_git_files
+    generate('gitignore', '.gitignore')
+    generate('gitattributes', '.gitattributes')
+  end
+
+  private
+
+  def generate(template, destination = template)
+    puts "Writing #{destination}"
+    existing_content = existing_file_contents(destination)
+
+    move_template(template, destination)
+
+    if existing_content
+      append_original_file_contents(destination, existing_content)
+    end
+  end
+
+  def existing_file_contents(filename)
+    if File.exists? filename
+      puts "#{filename} already exists!"
+      puts 'We will append the contents of the existing file to the end of the template'
+      File.read(filename).split("\n")
+    end
+  end
+
+  def move_template(template, destination)
+    template_path = "#{lib_dir}/templates/#{template}"
+    FileUtils.copy(template_path, destination, {})
+  end
+
+  def append_original_file_contents(filename, original_contents)
+    File.open(filename, 'a') do |file|
+      file.write("\n")
+      file.write("# Original #{filename} contents")
+      file.write("\n")
+      file.write(original_contents.join("\n"))
+    end
+  end
+
+  def lib_dir
+    @lib_dir ||= File.expand_path(File.dirname(File.dirname(__FILE__)))
+  end
+end
