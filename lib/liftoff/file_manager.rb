aa5f086f6447aad8212ceb5097c1972ca21e820b
Gordon Fontenot <gordon@thoughtbot.com>
2014-02-08 17:33:04 -0500

    Wrap classes in Liftoff module

--- lib/liftoff/file_manager.rb
+++ lib/liftoff/file_manager.rb
@@ -1,45 +1,47 @@
 require 'fileutils'
 
-class FileManager
-  def create_git_files
-    generate('gitignore', '.gitignore')
-    generate('gitattributes', '.gitattributes')
-  end
-
-  private
-
-  def generate(template, destination = template)
-    puts "Writing #{destination}"
-    existing_content = existing_file_contents(destination)
-
-    move_template(template, destination)
-
-    if existing_content
-      append_original_file_contents(destination, existing_content)
+module Liftoff
+  class FileManager
+    def create_git_files
+      generate('gitignore', '.gitignore')
+      generate('gitattributes', '.gitattributes')
     end
-  end
 
-  def existing_file_contents(filename)
-    if File.exists? filename
-      puts "#{filename} already exists!"
-      puts 'We will append the contents of the existing file to the end of the template'
-      File.read(filename)
+    private
+
+    def generate(template, destination = template)
+      puts "Writing #{destination}"
+      existing_content = existing_file_contents(destination)
+
+      move_template(template, destination)
+
+      if existing_content
+        append_original_file_contents(destination, existing_content)
+      end
     end
-  end
 
-  def move_template(template, destination)
-    template_path = File.join(templates_dir, template)
-    FileUtils.copy(template_path, destination, {})
-  end
-
-  def append_original_file_contents(filename, original_contents)
-    File.open(filename, 'a') do |file|
-      file.write("\n# Original #{filename} contents\n")
-      file.write(original_contents)
+    def existing_file_contents(filename)
+      if File.exists? filename
+        puts "#{filename} already exists!"
+        puts 'We will append the contents of the existing file to the end of the template'
+        File.read(filename)
+      end
     end
-  end
 
-  def templates_dir
-    File.expand_path('../../../templates', __FILE__)
+    def move_template(template, destination)
+      template_path = File.join(templates_dir, template)
+      FileUtils.copy(template_path, destination, {})
+    end
+
+    def append_original_file_contents(filename, original_contents)
+      File.open(filename, 'a') do |file|
+        file.write("\n# Original #{filename} contents\n")
+        file.write(original_contents)
+      end
+    end
+
+    def templates_dir
+      File.expand_path('../../../templates', __FILE__)
+    end
   end
 end
--- lib/liftoff/xcodeproj_helper.rb
+++ lib/liftoff/xcodeproj_helper.rb
@@ -26,98 +26,100 @@ HOSEY_WARNINGS = %w(
   CLANG_WARN_OBJC_IMPLICIT_RETAIN_SELF
 )
 
-class XcodeprojHelper
-  XCODE_PROJECTS = Dir.glob("*.xcodeproj")
+module Liftoff
+  class XcodeprojHelper
+    XCODE_PROJECTS = Dir.glob("*.xcodeproj")
 
-  def initialize
-    @project = Xcodeproj::Project.open(xcode_project_file)
-    @target = project_target
-  end
-
-  def treat_warnings_as_errors
-    say 'Setting GCC_TREAT_WARNINGS_AS_ERRORS for Release builds'
-    @target.build_settings('Release')['GCC_TREAT_WARNINGS_AS_ERRORS'] = 'YES'
-    save_changes
-  end
-
-  def enable_hosey_warnings
-    say 'Setting Hosey warnings at the project level'
-    @project.build_configurations.each do |configuration|
-      HOSEY_WARNINGS.each do |setting|
-        configuration.build_settings[setting] = 'YES'
-      end
-    end
-    save_changes
-  end
-
-  def enable_static_analyzer
-    say 'Turning on Static Analyzer at the project level'
-    @project.build_configurations.each do |configuration|
-      configuration.build_settings['RUN_CLANG_STATIC_ANALYZER'] = 'YES'
-    end
-    save_changes
-  end
-
-  def set_indentation_level(level)
-    say "Setting the project indentation level to #{level} spaces"
-    main_group = @project.main_group
-    main_group.indent_width = level.to_s
-    main_group.tab_width = level.to_s
-    main_group.uses_tabs = '0'
-    save_changes
-  end
-
-  def add_todo_script_phase
-    say 'Adding shell script build phase to warn on TODO and FIXME comments'
-    add_shell_script_build_phase(TODO_WARNING_SCRIPT, 'Warn for TODO and FIXME comments')
-  end
-
-  private
-
-  def project_target
-    @project_target ||= choose_item("target", available_targets)
-  end
-
-  def xcode_project_file
-    @xcode_project_file ||= choose_item('Xcode project', XCODE_PROJECTS)
-
-    if @xcode_project_file == 'Pods.xcodeproj'
-      raise 'Can not run in the Pods directory. $ cd .. maybe?'
+    def initialize
+      @project = Xcodeproj::Project.open(xcode_project_file)
+      @target = project_target
     end
 
-    @xcode_project_file
-  end
-
-  def choose_item(title, objects)
-    if objects.empty?
-      raise "Could not locate any #{title}s!"
-    elsif objects.size == 1
-      objects.first
-    else
-      choose("Which #{title} would you like to modify?") do |menu|
-        menu.index = :number
-        objects.map { |object| menu.choice(object) }
-      end
-    end
-  end
-
-  def available_targets
-    @project.targets.to_a.delete_if { |t| t.name.end_with?('Tests') }
-  end
-
-  def add_shell_script_build_phase(script, name)
-    unless build_phase_exists_with_name name
-      build_phase = @target.new_shell_script_build_phase(name)
-      build_phase.shell_script = script
+    def treat_warnings_as_errors
+      say 'Setting GCC_TREAT_WARNINGS_AS_ERRORS for Release builds'
+      @target.build_settings('Release')['GCC_TREAT_WARNINGS_AS_ERRORS'] = 'YES'
       save_changes
     end
-  end
 
-  def build_phase_exists_with_name(name)
-    @target.build_phases.to_a.index { |phase| phase.display_name == name }
-  end
+    def enable_hosey_warnings
+      say 'Setting Hosey warnings at the project level'
+      @project.build_configurations.each do |configuration|
+        HOSEY_WARNINGS.each do |setting|
+          configuration.build_settings[setting] = 'YES'
+        end
+      end
+      save_changes
+    end
 
-  def save_changes
-    @project.save xcode_project_file
+    def enable_static_analyzer
+      say 'Turning on Static Analyzer at the project level'
+      @project.build_configurations.each do |configuration|
+        configuration.build_settings['RUN_CLANG_STATIC_ANALYZER'] = 'YES'
+      end
+      save_changes
+    end
+
+    def set_indentation_level(level)
+      say "Setting the project indentation level to #{level} spaces"
+      main_group = @project.main_group
+      main_group.indent_width = level.to_s
+      main_group.tab_width = level.to_s
+      main_group.uses_tabs = '0'
+      save_changes
+    end
+
+    def add_todo_script_phase
+      say 'Adding shell script build phase to warn on TODO and FIXME comments'
+      add_shell_script_build_phase(TODO_WARNING_SCRIPT, 'Warn for TODO and FIXME comments')
+    end
+
+    private
+
+    def project_target
+      @project_target ||= choose_item("target", available_targets)
+    end
+
+    def xcode_project_file
+      @xcode_project_file ||= choose_item('Xcode project', XCODE_PROJECTS)
+
+      if @xcode_project_file == 'Pods.xcodeproj'
+        raise 'Can not run in the Pods directory. $ cd .. maybe?'
+      end
+
+      @xcode_project_file
+    end
+
+    def choose_item(title, objects)
+      if objects.empty?
+        raise "Could not locate any #{title}s!"
+      elsif objects.size == 1
+        objects.first
+      else
+        choose("Which #{title} would you like to modify?") do |menu|
+          menu.index = :number
+          objects.map { |object| menu.choice(object) }
+        end
+      end
+    end
+
+    def available_targets
+      @project.targets.to_a.delete_if { |t| t.name.end_with?('Tests') }
+    end
+
+    def add_shell_script_build_phase(script, name)
+      unless build_phase_exists_with_name name
+        build_phase = @target.new_shell_script_build_phase(name)
+        build_phase.shell_script = script
+        save_changes
+      end
+    end
+
+    def build_phase_exists_with_name(name)
+      @target.build_phases.to_a.index { |phase| phase.display_name == name }
+    end
+
+    def save_changes
+      @project.save xcode_project_file
+    end
   end
 end
