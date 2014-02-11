06b90581c58838092c379203faac944b7ff3070a
Gordon Fontenot <gordon@thoughtbot.com>
2014-02-11 14:27:09 -0500

    Minor improvements to ProjectConfiguration
    * Wrap class in the Liftoff module (Why the hell do I keep forgetting
      this?)
    * Move `ATTRIBUTES` constant to proper `attr_accessor` declaration

--- lib/liftoff/project_configuration.rb
+++ lib/liftoff/project_configuration.rb
@@ -1,16 +1,15 @@
-class ProjectConfiguration
+module Liftoff
+  class ProjectConfiguration
+    attr_accessor :git, :error, :todo, :staticanalyzer, :indentation, :warnings, :directories
 
-  ATTRIBUTES = %i(git error todo staticanalyzer indentation warnings directories)
-
-  attr_accessor *ATTRIBUTES
-
-  def initialize(hash)
-    hash.each_pair do |attr, val|
-      if respond_to?("#{attr}=")
-        send("#{attr}=", val)
-      else
-        STDERR.puts "Unknown key #{attr} found in liftoffrc!"
-        exit 1
+    def initialize(hash)
+      hash.each_pair do |attr, val|
+        if respond_to?("#{attr}=")
+          send("#{attr}=", val)
+        else
+          STDERR.puts "Unknown key #{attr} found in liftoffrc!"
+          exit 1
+        end
       end
     end
   end
