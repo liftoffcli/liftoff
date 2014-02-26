d654374a61a96291d9cd85d65d089feebdb8cca0
Gordon Fontenot <gordon@thoughtbot.com>
2014-02-26 14:25:30 -0500

    Update formula for master

--- liftoff.rb
+++ liftoff.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Liftoff < Formula
   homepage 'http://github.com/thoughtbot/liftoff'
   # This will change once we release. For now, I just want to point it at this branch since I know it works.
-  url 'https://github.com/thoughtbot/liftoff/raw/gf-vendorize-gems/pkg/Liftoff-0.7.6.tar.gz'
-  sha1 '7b30fe6e1946d8ef83eed305d489aa59a7de4275'
+  url 'https://github.com/thoughtbot/liftoff/raw/master/pkg/Liftoff-0.7.6.tar.gz'
+  sha1 '686de61f512dd9022f30e765cfd701425688ca72'
 
   depends_on 'xcproj' => :recommended
 
