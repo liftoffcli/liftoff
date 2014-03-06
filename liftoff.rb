62c739e92a0d768356a485d129fb4ef5e2856a82
Gordon Fontenot <gordon@thoughtbot.com>
2014-03-06 10:40:23 -0500

    Update tarball with recent changes

--- liftoff.rb
+++ liftoff.rb
@@ -4,7 +4,7 @@ class Liftoff < Formula
   homepage 'http://github.com/thoughtbot/liftoff'
   # This will change once we release. For now, I just want to point it at this branch since I know it works.
   url 'https://github.com/thoughtbot/liftoff/raw/master/pkg/Liftoff-0.7.6.tar.gz'
-  sha1 '686de61f512dd9022f30e765cfd701425688ca72'
+  sha1 '8afd0d04f85f8b5ddc11817957e1c9d558dfa34f'
 
   depends_on 'xcproj' => :recommended
 
