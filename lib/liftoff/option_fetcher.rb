88f55e4e1644269ec4e72512dcf2ffdf6e958bb0
Gordon Fontenot <gordon@thoughtbot.com>
2014-03-11 15:52:03 -0400

    Trap interrupt during option input
    * Extract OptionFetcher
      This class handles fetching the options from the user, and manages the
      various interrupt signals during input
    
    Fixes #81

---
+++ lib/liftoff/option_fetcher.rb
@@ -0,0 +1,26 @@
+module Liftoff
+  class OptionFetcher
+    def initialize(configuration)
+      @configuration = configuration
+    end
+
+    def fetch_options
+      fetch_option_for(:project_name, 'Project name')
+      fetch_option_for(:company, 'Company name')
+      fetch_option_for(:author, 'Author name')
+      fetch_option_for(:prefix, 'Prefix')
+    end
+
+    private
+
+    def fetch_option_for(attribute, prompt)
+      value = ask("#{prompt}? ") { |q| q.default = @configuration.public_send(attribute) }
+      @configuration.public_send("#{attribute}=", value)
+    rescue EOFError
+      puts
+      fetch_option_for(attribute, prompt)
+    rescue Interrupt
+      exit 1
+    end
+  end
+end
