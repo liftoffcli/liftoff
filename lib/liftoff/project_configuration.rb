42d7f8d2e8f5fc9b86b9d9fb6ef2bd34f355dd45
Gordon Fontenot <gordon@thoughtbot.com>
2014-03-20 16:28:39 -0400

    Add deprecation manager
    This class will handle the mappings between deprecated keys and their
    replacements.

--- lib/liftoff/project_configuration.rb
+++ lib/liftoff/project_configuration.rb
@@ -6,14 +6,16 @@ module Liftoff
     attr_writer :author, :company_identifier, :use_tabs
 
     def initialize(liftoffrc)
+      deprecations = DeprecationManager.new
       liftoffrc.each_pair do |attribute, value|
         if respond_to?("#{attribute}=")
           send("#{attribute}=", value)
         else
-          STDERR.puts "Unknown key #{attribute} found in liftoffrc!"
-          exit 1
+          deprecations.handle_key(attribute)
         end
       end
+
+      deprecations.finish
     end
 
     def author
