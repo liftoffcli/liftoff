cb22635fbf8b8be963c4cc57389572da245565c1
TheSooth <thesooth@aol.com>
2014-12-04 20:48:44 +0200

    updating swift AppDelegate header

--- templates/AppDelegate.swift
+++ templates/AppDelegate.swift
@@ -1,6 +1,16 @@
+//
+//  AppDelegate.swift
+//  <%= project_name %>
+//
+//  Created by <%= author %> on <%= Time.now.strftime("%-m/%-d/%y") %>
+//  Copyright (c) <%= Time.now.strftime('%Y') %> <%= company %>. All rights reserved.
+//
+
+
 import UIKit
 
 @UIApplicationMain
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 }
