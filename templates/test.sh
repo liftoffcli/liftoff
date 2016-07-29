148a0fa8ce24b03abc782fe1d4f2650cec6c0894
Jake Craige <james.craige@gmail.com>
2016-07-28 22:42:20 -0700

    Swap Travis configuration with Circle
    We use Circle as a default now because it seems to work better than the others.
    Since we don't use Travis anymore, this also makes it so we don't need to
    maintain a configuration for it.

--- templates/test.sh
+++ templates/test.sh
@@ -1,5 +1,45 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+# Runs tests via xcodebuild, formatting results with xcpretty.
+#
+# Usage: bin/test [options]
+#
+# Options:
+#   -k    Clean before running the tests.
+#   -r    Emit JUnit XML report.
+#   -t    Use RSpec-style test output.
 
-set -o pipefail
+set -eo pipefail
 
-xcodebuild test -workspace <%= project_name %>.xcworkspace -scheme <%= project_name %> -sdk iphonesimulator BUILD_ACTIVE_ARCH=NO | xcpretty -t -c
+build_actions=(test)
+test_reports_dir="${CIRCLE_TEST_REPORTS:-build}"
+xcpretty_options=(--color)
+
+while getopts krt opt; do
+  case ${opt} in
+    k)
+      build_actions=(clean test)
+      ;;
+    r)
+      xcpretty_options+=(--report junit --output "${test_reports_dir}/test-report.xml")
+      ;;
+    t)
+      xcpretty_options+=(--test)
+      ;;
+    *)
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
+mkdir -p "${test_reports_dir}"
+
+env NSUnbufferedIO=YES xcrun xcodebuild \
+  -workspace <%= project_name %>.xcworkspace \
+  -scheme <%= project_name %> \
+  -sdk iphonesimulator \
+  BUILD_ACTIVE_ARCH=NO \
+  "${build_actions[@]}" \
+  | xcpretty "${xcpretty_options[@]}"
